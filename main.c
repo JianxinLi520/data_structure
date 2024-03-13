6e83997e329607202c795ce5837ff05e77ac8096
LiJianxin <844485538@qq.com>
2024-03-13 17:04:01 +0800

    feat: 新增菜单

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+#define ARRAY_LENGTH(arr) (sizeof(arr) / sizeof(arr[0]))
+
+const char* type[] = {"1.顺序表\n", "2.单链表\n", "3.双向链表\n"};
+
+
 int main() {
-    printf("Hello, World!\n");
+    printf("请选择以下数据类型：\n");
+    for (int i=0; i<ARRAY_LENGTH(type); i++){
+        printf("%s", type[i]);
+    }
+
+    char select[] = "";
+    scanf("%s", select);
+
+    printf("已选择类型：%s\n", select);
     return 0;
 }
+
+
+
